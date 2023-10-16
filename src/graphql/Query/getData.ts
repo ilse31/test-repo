36c2063612679ff7c8ae2865e84ad1873cd24664
ilse31 <ilhamprazsetyo33@gmail.com>
2023-10-16 23:52:51 +0700

    add new query for get data

--- src/graphql/Query/getData.ts
+++ src/graphql/Query/getData.ts
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
-const getData = gql`
+
+//get contact list. need variables for searching
+const GET_CONTACT_LIST = gql`
   query GetContactList(
     $distinct_on: [contact_select_column!]
     $limit: Int
@@ -24,4 +26,47 @@ const getData = gql`
     }
   }
 `;
-export { getData };
+
+
+//GET Contact detail. Need id for fetch data
+const GET_CONTACT_DETAIL = gql`
+  query GetContactDetail($id: Int!) {
+    contact_by_pk(id: $id) {
+      last_name
+      id
+      first_name
+      created_at
+      phones {
+        number
+      }
+    }
+  }
+`;
+
+//GET PHONE LIST with variable for searching
+const GET_PHONE_LIST = gql`
+  query GetPhoneList(
+    $where: phone_bool_exp
+    $distinct_on: [phone_select_column!]
+    $limit: Int = 10
+    $offset: Int = 0
+    $order_by: [phone_order_by!]
+  ) {
+    phone(
+      where: $where
+      distinct_on: $distinct_on
+      limit: $limit
+      offset: $offset
+      order_by: $order_by
+    ) {
+      contact {
+        last_name
+        first_name
+        id
+      }
+      number
+    }
+  }
+`;
+
+export { GET_CONTACT_LIST, GET_CONTACT_DETAIL, GET_PHONE_LIST };
